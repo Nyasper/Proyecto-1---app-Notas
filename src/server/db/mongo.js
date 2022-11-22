edea1471c36e817ab648e60184908ae06c74217a
Nyasper <herreragonzalo39@gmail.com>
2022-11-21 21:59:54 -0300

    Configurado ENV

--- src/server/db/mongo.js
+++ src/server/db/mongo.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
+dotenv.config({path:'../../.env'})
 
 export async function connect(){
     try {
-        await mongoose.connect('mongodb+srv://gonroot:C9jsaZcF@cluster0.achdeii.mongodb.net/notas')
-    console.log('Connected success to MongoDB')
+        await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.achdeii.mongodb.net/notas')
+    console.log('Connected success to MongoDB`)
     } catch (error) {
         console.log(error)
     }
